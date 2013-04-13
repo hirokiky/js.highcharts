f780b56c0272253175e6f3db1f009a10b8a38c00
hirokiky <hirokiky@gmail.com>
2013-04-14 04:20:42 +0900

    Fixed bug, the js file was not ungzipped

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
+import cStringIO
+import gzip
 import os
+
 try:
-    from urllib import urlretrieve
+    from urllib import urlopen
 except ImportError:
-    from urllib.request import urlretrieve
+    from urllib.request import urlopen
 
 from setuptools import setup, find_packages
 from setuptools.command.install import install as _install
@@ -14,11 +17,18 @@ requires = [
     'fanstatic',
     ]
 
+def download_file(url, path):
+    usock = urlopen(url)
+    data = usock.read()
+    if usock.headers.get('content-encoding', None) == 'gzip':
+        data = gzip.GzipFile(fileobj=cStringIO.StringIO(data)).read()
+    open(path, 'w').write(data)
+
 class install(_install):
     def run(self):
         _install.run(self)
         resources_dir = os.path.join(self.install_platlib, resources_prefix)
-        urlretrieve(highcharts_url, resources_dir)
+        download_file(highcharts_url, resources_dir)
 
 setup(name='js.highcharts',
       version='0.0',
